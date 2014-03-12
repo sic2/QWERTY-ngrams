0d6cc3b5594ca048b1251e1f48ceff7c2bb4d4c3
Simone Ivan Conte <sic2@st-andrews.ac.uk>
2014-03-12 21:22:34 +0000

    README updated + basic tests + report

--- README.md
+++ README.md
@@ -7,4 +7,27 @@ Read report.pdf for a more detailed description of the project.
 
 ## Instructions
 
-TODO
+You can run one of the pre-set tests in keyboardCorrection.py by uncommenting the test and running:
+
+	$ python keyboardCorrection.py
+
+Note that test4-7 can take a considerably amount of time (>1hr).
+
+Additional tests can also be easily written, as long as the following steps are followed:
+
+1. A model object is created
+	model = ngramsModel()
+
+2. A confusion matrix is created
+	model.generateSTDConfusionMatrix()
+
+	or 
+
+	model.readConfusionMatrix('customKeyboard.csv')
+
+3.1 Either an N-gram model is explicitly created and used
+	model.createNgramModel(N, corpus)
+	model.proposeCorrection(WORD, lastKeyOnly = True/False, printOutput=True/False)
+
+3.2 Or CV is performed
+	model.performCV(corpus, N, k = (>2), lastKeyOnly = True/False)
--- keyboardCorrection.py
+++ keyboardCorrection.py
@@ -1,14 +1,62 @@
 #!/usr/bin/python
 from ngramsModel import ngramsModel
 from nltk.corpus import brown
-from random import random
+
+def test0():
+	model = ngramsModel()
+	model.generateSTDConfusionMatrix()
+	model.createNgramModel(3, brown.words())
+	print model.proposeCorrection("hellp", lastKeyOnly = True, printOutput=True)
+
+def test1():
+	model = ngramsModel()
+	model.readConfusionMatrix('customKeyboard.csv')
+	model.createNgramModel(3, brown.words())
+	print model.proposeCorrection("hellp", lastKeyOnly = True, printOutput=True)
+
+def test2():
+	model = ngramsModel()
+	model.generateSTDConfusionMatrix()
+	model.createNgramModel(3, brown.words())
+	print model.proposeCorrection("hdllp", lastKeyOnly = False, printOutput=True)
+
+def test3():
+	model = ngramsModel()
+	model.readConfusionMatrix('customKeyboard.csv')
+	model.createNgramModel(3, brown.words())
+	print model.proposeCorrection("hdllp", lastKeyOnly = False, printOutput=True)
+
+# The tests below can take a considerably amount of time (> 1hr)
+# Using brown.words()[:10000] will reduce the total running time, 
+# but remember that this will lead to weaker (and partial) models.
+def test4():
+	model = ngramsModel()
+	model.generateSTDConfusionMatrix()
+	model.performCV(brown.words(), 6, k = 10, lastKeyOnly = True)
+
+def test5():
+	model = ngramsModel()
+	model.readConfusionMatrix('customKeyboard.csv')
+	model.performCV(brown.words(), 6, k = 10, lastKeyOnly = True)
+
+def test6():
+	model = ngramsModel()
+	model.generateSTDConfusionMatrix()
+	model.performCV(brown.words(), 6, k = 10, lastKeyOnly = False)
+
+def test7():
+	model = ngramsModel()
+	model.readConfusionMatrix('customKeyboard.csv')
+	model.performCV(brown.words(), 6, k = 10, lastKeyOnly = False)
 
 def main():
-    model = ngramsModel() 
-    #model.generateSTDConfusionMatrix()
-    model.readConfusionMatrix('customKeyboard.csv')
-    model.performCV(brown.words(), 6, k = 10, lastKeyOnly = True)
-    #model.createNgramModel(3, brown.words()[:10000])
-    #print model.proposeCorrection("hello", lastKeyOnly = True, printOutput=True)
+    test0()
+    # test1()
+    # test2()
+    # test3()
+    # test4()
+    # test5()
+    # test6()
+    # test7()
 
 if  __name__ =='__main__':main()
